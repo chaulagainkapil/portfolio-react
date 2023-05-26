aaf8324b82bbfdf40ba4b80b130d8e2a15b0b751
chaulagainkapil <chaukapil99@gmail.com>
2023-05-26 12:59:56 +0545

    homepage done!

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,72 @@
-import LogoTitle from '../../assets/images/logo-s.png';
-import { Link } from 'react-router-dom';
-import './index.scss';
-const Home = () => {
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+import { useState, useEffect } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from '../Home/Logo'
 
-    return(
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>Hi, <br/> I am
-                <img src={LogoTitle} alt="developer"/>
-                Lorem Ipsum <br /> web developer</h1>
-                <br />
-                <h2>Frontend Developer/ Web Developer/ Web Designer</h2>
-                {/* AT 31:50 of video */}
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-            </div>
-        </div>
-    )
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['L', 'o', 'r', 'e', 'm']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    '',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+  
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+  
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>{' '}
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Frontend Developer/ Web Developer/ Web Designer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+      <Logo />
+
+    </div>
+  )
 }
 
 export default Home
